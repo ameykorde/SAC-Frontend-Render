5cd68288f6e14e45b41790cea87a04a46214f871
ameykorde <ameykorde1@gmail.com>
2023-05-29 11:39:05 +0530

    change to render backend

--- services/url.jsx
+++ services/url.jsx
@@ -1,4 +1,4 @@
 // export const BASE_URL = 'http://localhost:3000'
+// export const BASE_URL = 'https://teal-helpful-crayfish.cyclic.app'
 // export const BASE_URL = 'https://bsckendrailway-production.up.railway.app'
-// export const BASE_URL = 'https://smartactivitycalendar.onrender.com'
-export const BASE_URL = 'https://teal-helpful-crayfish.cyclic.app'
+export const BASE_URL = 'https://smartactivitycalendar.onrender.com'
